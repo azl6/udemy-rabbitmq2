26c11041808107fbd001a7b180d331c3ec8ef00d
Alex <alex.rodrigues23997@gmail.com>
2022-04-25 00:44:01 -0300

    Initial commit

---
+++ producertwo/src/main/java/rabbitmq/producertwo/ProducertwoApplication.java
@@ -0,0 +1,13 @@
+package rabbitmq.producertwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducertwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducertwoApplication.class, args);
+	}
+
+}
---
+++ producertwo/src/test/java/rabbitmq/producertwo/ProducertwoApplicationTests.java
@@ -0,0 +1,13 @@
+package rabbitmq.producertwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducertwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
